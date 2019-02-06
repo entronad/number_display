39690d03ba06477ff53bd958a96110a306f75ad3
entronad <entronad@gmail.com>
2019-02-06 21:56:50 +0800

    main

--- lib/number_display.dart
+++ lib/number_display.dart
@@ -1,7 +1,27 @@
 library number_display;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+String _commafy(String integerStr) => integerStr.replaceAllMapped(
+  RegExp(r'(\d{1,3})(?=(\d{3})+(?!\d))'),
+  (m) => '${m[1]},',
+);
+
+// [integer, decimal, unit]
+
+String display(
+  Object input,
+  int length,
+  {
+    int maxAccuracy = 2,
+    String placeholder = '--',
+    bool allowText = true,
+    bool comma = true,
+    List<String> units = const ['k', 'M', 'G', 'T'],
+    int unitsInterval = 3,
+    int unitsMaxAccuracy = 4,
+  }
+) {
+  // placeholder must in length
+  if (length < placeholder.length) {
+    throw Error();
+  }
 }
